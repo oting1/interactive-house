707f6ab4122c3d1f7c0ad35583aee8c7a2b32b1d
oting1 <31719584+oting1@users.noreply.github.com>
2017-09-06 22:51:43 -0700

    interactive clouds

--- sketch.js
+++ sketch.js
@@ -1,5 +1,10 @@
+var r,g,b;
+
 function setup() { 
   createCanvas(400, 400);
+  r=random(255);
+  g=random(255);
+  b=random(255);
 } 
 
 function draw() { 
@@ -14,10 +19,36 @@ function draw() {
   rect(135,115,30,60);
   rect(200,250,50,100);
   triangle(100, 200, 300, 200, 200, 100);
-  fill(244, 251, 252);
+  fill(r, g, b);
   noStroke()
   ellipse(135,90,20,20);
+  fill(r+10, g+100, b+100);
   ellipse(175,60,35,35);
+  fill(r+20, g+150, b+250);
   ellipse(250,60,60,60);
-  
+}
+
+function mousePressed() {
+  var d = dist(mouseX,mouseY,135,90);
+  if (d<10) 
+  {
+     r=random(255);
+     g=random(255);
+     b=random(255);
+  }
+  var d = dist(mouseX,mouseY,175,60);
+  if (d<18)
+    {
+     r=random(255);
+     g=random(255);
+     b=random(255);
+  }
+    var d = dist(mouseX,mouseY,250,60);
+  if (d<30)
+    {
+     r=random(255);
+     g=random(255);
+     b=random(255);
+  }
+
 }
